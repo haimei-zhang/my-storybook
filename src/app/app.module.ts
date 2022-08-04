3b154ae5980e24f0abb3cc6e70d02679202500d2
haimei.zhang <haimei.zhang@oneadvanced.com>
2022-08-04 01:44:47 +0100

    add home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './view/home/home.component';
-import { IntroductionComponent } from './view/introduction/introduction.component';
-import { AchievementsComponent } from './view/achievements/achievements.component';
-import { StoriesComponent } from './view/stories/stories.component';
+
+import { HomeComponent } from '~view/home/home.component';
+import { IntroductionComponent } from '~view/introduction/introduction.component';
+import { AchievementsComponent } from '~view/achievements/achievements.component';
+import { StoriesComponent } from '~view/stories/stories.component';
 
 @NgModule({
   declarations: [
--- src/app/view/home/home.component.ts
+++ src/app/view/home/home.component.ts
@@ -12,4 +12,8 @@ export class HomeComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  alert() {
+    alert('1')
+  }
+
 }
